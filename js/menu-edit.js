0e4793c3c00b93368d8f84d172a334d2b3732a98
bombiux <isaiassalvador91@gmail.com>
2021-03-07 22:45:37 -0600

    cuarto

--- js/menu-edit.js
+++ js/menu-edit.js
@@ -161,7 +161,7 @@ document.getElementById("type").onchange = function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /mnt/f/Proyects/laravel-coreui/resources/js/coreui/menu-edit.js */"./resources/js/coreui/menu-edit.js");
+module.exports = __webpack_require__(/*! /mnt/f/Proyects/minim-website/resources/js/coreui/menu-edit.js */"./resources/js/coreui/menu-edit.js");
 
 
 /***/ })
